dab458d616fe164dbf108d0c7af92b1ff46c9783
maxw3b <146854029+maxw3b@users.noreply.github.com>
2025-09-18 18:08:09 -0300

    Add files via upload

---
+++ java-main/JavaScriptone-main/index.js
@@ -0,0 +1,10 @@
+document.getElementById("texto").innerHTML="Meu primeiro texto <b>JS/b>!";
+console.log("Oi sso é um console.log");
+let a,b,c;
+a=5;
+b=6;
+c=7;      
+let pessoa= "dimitri";
+function minhafuncao(){
+document.write(5+5);
+}
